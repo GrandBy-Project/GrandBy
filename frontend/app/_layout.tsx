3b6d62d0c5bd9baf3c1a5014b937b936eafc2466
조수민 <skg04064@gmaile.com>
2025-10-13 19:23:20 +0900

    <기능>(일정)할 일 상세보기, 일정 등록, 캘린더  화면 구현 후 푸시합니다.

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout() {
       <Stack.Screen name="mypage" />
       <Stack.Screen name="settings" />
       <Stack.Screen name="todos" />
+      <Stack.Screen name="todo-detail" />
+      <Stack.Screen name="todo-write" />
+      <Stack.Screen name="calendar" />
     </Stack>
   );
 }
