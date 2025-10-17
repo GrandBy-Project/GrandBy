664031671981104376358cd9850c5aca54552a85
jhpark0623 <as2248@naver.com>
2025-10-17 19:03:19 +0900

    <db>: migration 엉킨 문제 해결

--- backend/migrations/versions/20251017_0000-add_conversation_summary.py
+++ backend/migrations/versions/20251017_0000-add_conversation_summary.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'f1a2b3c4d5e6'
-down_revision: Union[str, None] = 'bbc4f5258de3'
+down_revision: Union[str, None] = 'b1c2d3e4f5g6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
